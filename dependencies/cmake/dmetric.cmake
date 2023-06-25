a5a07680b6ef21c3f41046db8ce1cf70fb8c9649
mrchaohuang <mrchaohuang@tencent.com>
2023-06-25 16:06:50 -0700

    change dmetric repo url

--- dependencies/cmake/dmetric.cmake
+++ dependencies/cmake/dmetric.cmake
@@ -5,7 +5,7 @@ include( ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/cmake/CPM.cmake )
 set( DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/dmetric )
 if( NOT EXISTS ${DIR} )
   CPMAddPackage( NAME             dmetric
-                GIT_REPOSITORY    http://mpegx.int-evry.fr/software/MPEG/PCC/mpeg-pcc-dmetric.git
+                GIT_REPOSITORY    https://git.code.tencent.com/mrchaohuang/mirror_dmetric.git
                 GIT_TAG           release-v0.14.1
                 SOURCE_DIR        ${DIR}
                 DOWNLOAD_ONLY     YES )
