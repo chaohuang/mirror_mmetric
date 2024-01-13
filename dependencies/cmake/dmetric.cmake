a5c3a869c662282c427adf341253072fb10023d2
Chao Huang <mrchaohuang@tencent.com>
2024-01-12 20:59:17 -0800

    change dmetric repo

--- dependencies/cmake/dmetric.cmake
+++ dependencies/cmake/dmetric.cmake
@@ -5,7 +5,7 @@ include( ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/cmake/CPM.cmake )
 set( DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/dmetric )
 if( NOT EXISTS ${DIR} )
   CPMAddPackage( NAME             dmetric
-                GIT_REPOSITORY    https://git.code.tencent.com/mrchaohuang/mirror_dmetric.git
+                GIT_REPOSITORY    https://github.com/chaohuang/mirror_dmetric.git
                 GIT_TAG           release-v0.14.1-for_macos
                 SOURCE_DIR        ${DIR}
                 DOWNLOAD_ONLY     YES )
